c656ca252955cae4a12122dc68cdfcf218ff1710
Binyam Tekae <115328563+binyamtekae@users.noreply.github.com>
2023-04-13 19:34:22 -0400

    pushing some update

---
+++ DemoProject/script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+const btn = document.querySelector(".btn-open");
+const form= document.querySelector(".fact-form");
+
+btn.addEventListener('click', function(){
+   if(form.classList.contains("hidden")){
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+   }
+   else{
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+   }
+});
+
+
+const text = "lisbon is the capital of portugal"; 
